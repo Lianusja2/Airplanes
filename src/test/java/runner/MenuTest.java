b57d8ca1502671b5ba0a40ddfd48f11f91f835a8
Lianusja2 <Lianusja2@yandex.ru>
2018-02-20 21:56:45 +0200

    minor fixes

--- src/test/java/runner/MenuTest.java
+++ src/test/java/runner/MenuTest.java
@@ -3,14 +3,11 @@ package runner;
 import org.junit.Before;
 import org.junit.Test;
 import plane_Owner.Company;
-import plane_Owner.CompanyService;
-import plane_Owner.CorporationService;
 import planes.CargoPlane;
 import planes.CivilAircraft;
 import planes.CivilAircraftType;
 import planes.PassengersPlane;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -22,17 +19,13 @@ import static org.mockito.Mockito.when;
 
 public class MenuTest {
     private Company mau;
-    private CorporationService companyService;
     private CivilAircraft cargoPlane, passengerPlane;
     private List<Company> companies;
-    private String fileName = "TestCompaniesList";
-    private File file = new File(fileName);
     private Menu menu;
     private Scanner sc;
 
     @Before
     public void preConditions() throws IllegalAccessException {
-        companyService = new CompanyService();
         mau = new Company("Mau");
         passengerPlane = new PassengersPlane("TYY-177", 32, 4900.0, CivilAircraftType.COMMERCIAL, 100, 5);
         cargoPlane = new CargoPlane("WWU-YYY", 45.67, 5678.0, CivilAircraftType.COMMERCIAL, 10000, 2);
@@ -49,7 +42,7 @@ public class MenuTest {
     @Test
     public void filterByFuelUsageTest() {
         when(sc.next()).thenReturn(String.valueOf(32));
-        assertThat(menu.filterByFuelUsage(sc,mau), is(Arrays.asList(passengerPlane)));
+        assertThat(menu.filterByFuelUsage(sc, mau), is(Arrays.asList(passengerPlane)));
     }
 
     @Test
@@ -60,7 +53,7 @@ public class MenuTest {
 
     @Test
     public void continueInquiringInformationWhenValidInfoFalseTest() {
-      assertThat(menu.continueInquiringInformation(sc, mau, false), is(false));
+        assertThat(menu.continueInquiringInformation(sc, mau, false), is(false));
     }
 
     @Test
